4815a8bf6878e4e5f620c52029aa77f98a1c9b90
jarv1ssa <emin.aliyev@stu.khas.edu.tr>
2021-12-09 18:06:51 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const PORT = 5000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Server listening on " + PORT);
+});
