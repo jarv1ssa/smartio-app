5cadbdf84a85305c63d43f202be5ab1b422bc8f9
jarv1ssa <emin.aliyev@stu.khas.edu.tr>
2021-12-09 19:02:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", async (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => {
